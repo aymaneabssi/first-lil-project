e054d7198af86d8077f178ed5e4f6275a467de61
aymaneabssi <78247123+aymaneabssi@users.noreply.github.com>
2021-03-23 23:30:03 +0100

    fixed the cards order

--- src/App.js
+++ src/App.js
@@ -2,34 +2,34 @@ import './App.css';
 import NavBar from './conpoments/navBar'
 import Welcom from './conpoments/Welcom'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Card,CardGroup} from 'react-bootstrap';
+import {Card,Button,Container,Row,Col} from 'react-bootstrap';
 import Data from './data.json';
 import Footer from './conpoments/footer';
+import EllipsisText from "react-ellipsis-text";
+
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Welcom/>
       <div className="posts">
-        
-      <CardGroup/>
-  {
-          Data.map(book=>{
-            return (
-          <Card className="d-inline">
-          <Card.Img variant="top" src={book.img} style={{ width: '10rem' }}/>
-          <Card.Body>
-            <Card.Title>{book.title}</Card.Title>
-            <Card.Text>
-              price: {book.price}€ <br/>
-              category: {book.category}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-            )})}          
-         <CardGroup/>
-      </div>
-              <Footer className="Footer"/>
+      <Container>
+        <Row>
+          {Data.map((data) => (
+            <Col xs={4} sm={4} md={3} lg={2}>
+            <Card style={{ width: "10rem" ,height: "auto"}} className="my-3 d-flex justify-content-between">
+              <Card.Img style={{ width: "10rem", height: "15rem" }} variant="top" src={data.img} />
+              <Card.Body>
+                <Card.Title><EllipsisText text={data.title} length={"15"} /></Card.Title>
+                <Card.Text>{data.price} €</Card.Text>
+                <Button variant="primary">BUY!</Button>
+              </Card.Body>
+            </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </div>
     </div>
   );
 }
